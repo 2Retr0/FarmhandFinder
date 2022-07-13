84fced5210c4ec5106f053b3f8eb5fc6b32d07b9
2Retr0 <18603664+2Retr0@users.noreply.github.com>
2022-07-12 22:57:32 -0700

    Added basic mod configuration.
    Configuration includes:
        * Disabling the compass bubble.
        * Disabling the compass arrow.

---
+++ ModConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace FarmhandFinder
+{
+    public class ModConfig
+    {
+        public bool HideCompassBubble { get; set; } = false;
+        public bool HideCompassArrow { get; set; } = false;
+    }
+}
